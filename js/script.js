e748a27c2e364d146c2711a719a5ed62cec24be5
Fantom <fantomcheg25@gmail.com>
2020-12-27 13:37:16 +0500

    27.12.2020 13:37 первая контрольная точка все работает

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+/* //const answer=prompt("Вы здесь?","");
+ //console.log(typeof(answer));
+
+/*<!--let number=5;
+/* const leftBorderWith=2;
+/* //number=10;
+//console.log(number);
+
+const person = {
+    name:"John",
+    age:34,
+    isMarried: true
+};
+
+//console.log(person.name);
+console.log(person["age"]);
+const answers=[];
+answers[0]=prompt("Как ваше имя?","");
+answers[1]=prompt("Как ваша фамилия?","");
+answers[2]=+prompt("Сколько Вам лет?","");
+document.write(answers); */
+
+const category = 'toys';
+console.log(4+  +"6");
